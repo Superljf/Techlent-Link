c64802e2ffb5a1a56d54ff27581f7e7d9fc02e9e
liujf <2718910554@qq.com>
2024-06-27 23:22:46 +0800

    refactor--use vite module

--- src/popup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Popup from '../public/popup.html';
-
-ReactDOM.render(
-  <Popup />,
-  document.getElementById('root')
-);
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'extension',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/[name].js',
+        entryFileNames: 'static/[name].js',
+        assetFileNames: 'static/[name].[ext]',
+      },
+    },
+  },
+  server: {
+    port: 3000, // 设置开发服务器端口
+    proxy: {
+      // 配置代理
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
