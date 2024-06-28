dd184a99c6c63c1e4bc36d0aadb89ae61dd31d5e
liujianfeng <jianfeng.liu@techlent.net>
2024-06-28 13:53:28 +0800

    feat-Tab国内&&国外地址

---
+++ src/App/index.tsx
@@ -0,0 +1,146 @@
+import React, { useEffect, useState } from 'react';
+import './index.less'; // 导入自定义的样式文件
+
+export default function App() {
+  const [linkList, setLinkList] = useState([
+    {
+      tab: '国内',
+      urlList: [
+        { pathname: '官网手机端测试环境', url: 'http://m.dev.techlent.com.cn' },
+        { pathname: '官网手机端正式环境', url: 'https://m.techlent.com.cn' },
+        { pathname: '官网Web测试环境', url: 'http://dev.techlent.com.cn' },
+        { pathname: '官网Web正式环境', url: 'https://techlent.com.cn' },
+      ],
+    },
+    {
+      tab: '国外',
+      urlList: [
+        { pathname: '官网手机端测试环境', url: 'https://mdev.techlent.net' },
+        { pathname: '官网手机端正式环境', url: 'https://m.techlent.com' },
+        { pathname: '官网Web测试环境', url: 'https://dev.techlent.net/' },
+        { pathname: '官网Web正式环境', url: 'https://techlent.com/cn' },
+      ],
+    },
+  ]);
+
+  const [newTab, setNewTab] = useState('');
+  const [newAddressName, setNewAddressName] = useState('');
+  const [newAddressURL, setNewAddressURL] = useState('');
+
+  const saveToLocalStorage = (updatedLinkList: any) => {
+    localStorage.setItem('linkListTechlent2024', JSON.stringify(updatedLinkList));
+  };
+
+  // useEffect(() => {
+  //   // 从 localStorage 中读取 linkList
+  //   const storedLinkList = localStorage.getItem('linkListTechlent2024');
+
+  //   if (storedLinkList !== "[]" && !storedLinkList) {
+  //     setLinkList(JSON.parse(storedLinkList));
+  //   } else {
+  //     // 如果 localStorage 中没有存储 linkList，则保存当前的 linkList 到 localStorage
+  //     saveToLocalStorage(linkList);
+  //   }
+  // }, []); // 空数组作为依赖，只在组件挂载时执行一次
+
+
+  const handleClick = (url) => {
+    window.open(url, '_blank');
+  };
+
+  const addNewTab = () => {
+    if (newTab.trim() === '') return;
+    const updatedLinkList = [...linkList, { tab: newTab, urlList: [] }];
+    setLinkList(updatedLinkList);
+    saveToLocalStorage(updatedLinkList);
+    setNewTab('');
+  };
+
+  const deleteTab = (tabIndex: any) => {
+    const updatedLinkList = [...linkList];
+    updatedLinkList.splice(tabIndex, 1);
+    setLinkList(updatedLinkList);
+    saveToLocalStorage(updatedLinkList);
+  };
+
+  const addNewAddress = (tabIndex: any) => {
+    if (newAddressName.trim() === '' || newAddressURL.trim() === '') return;
+    const updatedLinkList = linkList.map((item, index) => {
+      if (index === tabIndex) {
+        return {
+          ...item,
+          urlList: [...item.urlList, { pathname: newAddressName, url: newAddressURL }],
+        };
+      }
+      return item;
+    });
+    setLinkList(updatedLinkList);
+    saveToLocalStorage(updatedLinkList);
+    setNewAddressName('');
+    setNewAddressURL('');
+  };
+
+  const deleteAddress = (tabIndex: any, addressIndex: number) => {
+    const updatedLinkList = linkList.map((item, index) => {
+      if (index === tabIndex) {
+        const updatedUrlList = [...item.urlList];
+        updatedUrlList.splice(addressIndex, 1);
+        return {
+          ...item,
+          urlList: updatedUrlList,
+        };
+      }
+      return item;
+    });
+    setLinkList(updatedLinkList);
+    saveToLocalStorage(updatedLinkList);
+  };
+
+  return (
+    <div className="main">
+      {linkList?.map((item, index) => (
+        <div className="tabContainer" key={index}>
+          <div className="tabTitle">{item.tab}</div>
+          {item.urlList?.map((url, urlIndex) => (
+            <div key={url.url} className="addressContainer">
+              <div className="addressName" onClick={() => handleClick(url.url)}>{url.pathname}</div>
+              {/* <div className="deleteButton" onClick={() => deleteAddress(index, urlIndex)}>删除地址</div> */}
+            </div>
+          ))}
+          {/* <div className="formContainer">
+            <div style={{ flex: 1, display: 'flex', }}>
+              <input
+                className="inputField"
+                style={{ width: 90 }}
+                type="text"
+                placeholder="地址名称"
+                value={newAddressName}
+                onChange={(e) => setNewAddressName(e.target.value)}
+              />
+              <input
+                className="inputField"
+                style={{ width: 90 }}
+                type="text"
+                placeholder="地址URL"
+                value={newAddressURL}
+                onChange={(e) => setNewAddressURL(e.target.value)}
+              />
+            </div>
+            <button className="addButton" onClick={() => addNewAddress(index)}>添加地址</button>
+          </div> */}
+          {/* <div className="deleteTabButton" onClick={() => deleteTab(index)}>删除tab</div> */}
+        </div>
+      ))}
+      {/* <div className="addTabContainer">
+        <input
+          className="inputField"
+          type="text"
+          placeholder="新tab名称"
+          value={newTab}
+          onChange={(e) => setNewTab(e.target.value)}
+        />
+        <button className="addButton" onClick={addNewTab}>添加新tab</button>
+      </div> */}
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+ReactDOM.createRoot(document.getElementById('root-techlent-20240828-1')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
